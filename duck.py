2ec05fe127ad2cc1be26f4dc943eb7cc4beb07f2
lex2007 <89134695+lex2007@users.noreply.github.com>
2021-08-19 10:12:01 +0700

    Add files via upload

---
+++ duck.py
@@ -0,0 +1,15 @@
+a=int(input("1 число \n"))
+b=int(input("2 число \n"))
+d=input("+ - * \n")
+if d == '+':
+    c=a+b
+    print(c)
+elif d == '-':
+    c=a-b
+    print(c)
+elif d=='/':
+    c=a/d
+    print(c)
+elif d == '*':
+    c = a*b
+    print(c)
---
+++ duck2.py
@@ -0,0 +1,5 @@
+x=input('Введите строку \n')
+if x[0] == 'a' and len(x) <= 10:
+    print("yes")
+else:
+    print("no")
